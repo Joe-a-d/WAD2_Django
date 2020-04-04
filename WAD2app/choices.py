6929259b7e7502783ca22cb817f6de88503afbd2
Joe-a-d <2334590D@student.gla.ac.uk>
2020-04-04 15:14:53 +0100

    temp pref choices fix

--- WAD2app/choices.py
+++ WAD2app/choices.py
@@ -1,5 +1,5 @@
 
 SIZES = [("ANY", "Any"),("S","Small"),("M","Medium"),("L","Large")]
-AGES = [("PUP", "Puppy"), (2, "1-2"), (3, "2-5"), (4, "5+")]
+AGES = [("PUP", "Puppy"), ("1-2", "1-2"), ("2-5", "2-5"), ("5+", "5+")]
 GENDERS = [("F", "Female"), ("M", "Male")]
 ENERGY = [("H", "Live Wire"), ("M", "Average"), ("L", "Couch Potato")]
